28ebb27122183368b426d5782d0522aff9dfe774
MZH6 <2362471311@qq.com>
2024-11-16 13:45:55 +0800

    前端上边距提交

--- web/src/components/ContentField.vue
+++ web/src/components/ContentField.vue
@@ -1,6 +1,6 @@
 <!--内容们写在这里-->
 <template>
-    <div class ="container">
+    <div class ="container  content-field">
         <div class="card">
             <div class ="card-body">
                 <slot></slot>   <!--之后要填充的内容放在这里面-->
@@ -18,5 +18,7 @@
 
 
 <style scoped>
-
+div.content-field{
+    margin-top:20px;
+}
 </style>
--- web/src/views/user/UserIndex.vue
+++ web/src/views/user/UserIndex.vue
@@ -1,9 +1,18 @@
 <template>
     <div>用户信息</div>
+    <ContentField>
+        用户信息
+    </ContentField>
 
 </template>
 
 <script>
+import ContentField from '@/components/ContentField.vue'
+export default {
+    components: {
+        ContentField
+    }
+}
 
 </script>
 
